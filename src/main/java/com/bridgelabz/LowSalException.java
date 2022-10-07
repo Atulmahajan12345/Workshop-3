afb051afdc58455e7e75c101e652c73fe6d9b88e
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-10-07 10:15:47 +0530

    Added:Create a employee data using exception

---
+++ src/main/java/com/bridgelabz/LowSalException.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+
+public class LowSalException extends Exception {
+
+    @Override
+    public String toString() {
+        return "Opps There is Low Balance Than 500 Reppues";
+               
+    }
+   
+   
+
+}
